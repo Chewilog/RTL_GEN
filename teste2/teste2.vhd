24b0a25581874dfc9284ed487030689ca0a4b4a9
lxm10 <lucasxmoura@gmail.com>
2022-12-20 21:14:11 -0300

    minor minor fixes

--- teste2/teste2.vhd
+++ teste2/teste2.vhd
@@ -9,7 +9,7 @@ entity teste2 is
      B: in std_logic_vector(7 downto 0);
      C: in std_logic_vector(7 downto 0);
      Z: out std_logic_vector(7 downto 0);
-     Z: out std_logic_vector(7 downto 0)   );
+     J: out std_logic_vector(7 downto 0)   );
 end teste2;
 
 architecture Behavioral of teste2 is
@@ -21,11 +21,11 @@ component addsub8 is
            Y : out STD_LOGIC_VECTOR (7 downto 0));
 end component;
 
-signal sY_to_A2090 :STD_LOGIC_VECTOR( 7 DOWNTO 0 );
-constant sconst_to_op44311 : std_logic:='0';
-signal sY_to_out40413 :STD_LOGIC_VECTOR( 7 DOWNTO 0 );
-constant sconst_to_op88615 : std_logic:='1';
-signal sc_to_out52617 :std_logic_vector(7 downto 0);
+signal sY_to_A8340 :STD_LOGIC_VECTOR( 7 DOWNTO 0 );
+constant sconst_to_op18411 : std_logic:='0';
+signal sY_to_out41913 :STD_LOGIC_VECTOR( 7 DOWNTO 0 );
+constant sconst_to_op91615 : std_logic:='1';
+signal sc_to_out59217 :std_logic_vector(7 downto 0);
 
 begin
 
@@ -33,25 +33,25 @@ add1: addsub8
   port map(
      A=>A,
      B=>B,
-     OP=>sconst_to_op44311,
-     Y=>sY_to_A2090);
+     OP=>sconst_to_op18411,
+     Y=>sY_to_A8340);
 
 sub1: addsub8
   port map(
-     A=>sY_to_A2090,
+     A=>sY_to_A8340,
      B=>C,
-     OP=>sconst_to_op88615,
-     Y=>sY_to_out40413);
+     OP=>sconst_to_op91615,
+     Y=>sY_to_out41913);
 
 
 
-process(sY_to_A2090,sin_to_B4828)
+process(sY_to_A8340,C,J)
 begin
-   sc_to_out52617<= unsigned(sY_to_A2090)+unsigned(sin_to_B4828);
+   sc_to_out59217<= unsigned(sY_to_A8340)+unsigned(C)+unsigned(J);
 end process;
 
-Z<=sY_to_out40413;
-Z<=sc_to_out52617;
-
+Z<=sY_to_out41913;
+J<=sc_to_out59217;
+ 
 
 end behavioral;
